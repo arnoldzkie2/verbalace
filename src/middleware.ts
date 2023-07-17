6559a8e40c874ed86552e3835519fba6cba5f662
arnoldzkie2 <arnoldzkie22@gmail.com>
2023-07-17 21:05:48 +0800

    changed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'cn', 'jp', 'kr', 'vn'],
+  locales: ['en', 'zh', 'ja', 'kr', 'vi'],
  
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: 'en'

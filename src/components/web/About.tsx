40e7a44c55fda9d18a04bb9c3436bf62f1635e35
arnoldzkie2 <arnoldzkie22@gmail.com>
2023-07-24 15:48:59 +0800

    fixed

--- src/components/web/About.tsx
+++ src/components/web/About.tsx
@@ -1,20 +1,97 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import { useTranslations } from 'next-intl';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const About: React.FC = ({ }) => {
 
     const t = useTranslations('about')
 
+    const [videoSize, setVideoSize] = useState({
+        width: 0,
+        height: 0
+    })
+
+    const updateVideoSize = () => {
+        const width = window.innerWidth;
+        if (width > 300 && width < 400) {
+            setVideoSize({
+                width: 290,
+                height: 280
+            })
+        } else if (width > 400 && width < 500) {
+            setVideoSize({
+                width: 390,
+                height: 300
+            });
+        } else if (width > 500 && width < 600) {
+            setVideoSize({
+                width: 490,
+                height: 330
+            });
+        } else if (width > 600 && width < 700) {
+            setVideoSize({
+                width: 585,
+                height: 360
+            })
+        } else if (width > 700 && width < 800) {
+            setVideoSize({
+                width: 685,
+                height: 400
+            })
+        } else if (width > 800 && width < 900) {
+            setVideoSize({
+                width: 775,
+                height: 430
+            })
+        } else if (width > 900 && width < 1000) {
+            setVideoSize({
+                width: 875,
+                height: 450
+            })
+        } else if (width > 1000 && width < 1100) {
+            setVideoSize({
+                width: 975,
+                height: 500
+            })
+        } else if (width > 1100 && width < 1200) {
+            setVideoSize({
+                width: 1075,
+                height: 550
+            })
+        } else if (width > 1200 && width < 1300) {
+            setVideoSize({
+                width: 1175,
+                height: 550
+            })
+        } else if (width > 1300) {
+            setVideoSize({
+                width: 1275,
+                height: 560
+            })
+        }
+        // You can add more conditions for different viewport widths if needed
+    };
+
+    useEffect(() => {
+        // Set the initial video size
+        updateVideoSize();
+
+        // Add an event listener to track viewport width changes
+        window.addEventListener('resize', updateVideoSize);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', updateVideoSize);
+        };
+    }, []);
+
     return (
         <section className='py-20 px-5 sm:px-10 md:px-16 lg:px-24 xl:px-36 2xl:px-44 flex items-center gap-10 md:gap-16 text-gray-700 flex-col' id='about'>
             <h1 className='text-3xl md:text-4xl lg:text-5xl font-light lg:font-extralight 2xl:hidden'>{t('h1')}</h1>
-            <div className='flex flex-col gap-5 sm:gap-10 md:gap-16 xl:gap-20 items-center 2xl:flex-row lg:justify-between'>
-                <video className='w-full 2xl:w-2/3 outline-none rounded-md' controls loop>
-                    <source src='/about.mp4' type='video/mp4' />
-                </video>
-                <aside className='flex flex-col items-center md:items-start gap-5 lg:gap-7 text-gray-600'>
+            <div className='flex flex-col gap-5 sm:gap-10 md:gap-16 xl:gap-20 items-center xl:items-end 2xl:flex-row lg:justify-between'>
+                <iframe width={videoSize.width} className='w-full' height={videoSize.height} src="https://www.youtube.com/embed/KdQkwAnzgEA" title="Verbalace Introduction" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+                <aside className='flex flex-col 2xl:w-1/2 items-center md:items-start gap-5 lg:gap-7 text-gray-600'>
                     <h1 className='text-3xl md:text-4xl lg:text-5xl font-light lg:font-extralight mb-5 pb-5 border-b hidden 2xl:block'>{t('h1')}</h1>
                     <p className='sm:leading-6 md:leading-7 text-sm xl:text-base'>{t('description.1')}</p>
                     <p className='sm:leading-6 md:leading-7 text-sm xl:text-base'>{t('description.2')}</p>
--- src/components/web/Services.tsx
+++ src/components/web/Services.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import React from 'react';
 
 interface ServicesProps {
@@ -13,27 +14,35 @@ const Services: React.FC<ServicesProps> = ({ }) => {
 
     return (
         <section id='services' className='py-20 flex items-center flex-col gap-10 lg:gap-16 px-5 sm:px-10 md:px-16 lg:px-24 xl:px-36 2xl:px-44'>
-            <h1 className='text-gray-700 text-3xl md:text-4xl lg:text-5xl font-light lg:font-extralight pb-8 mb-8 border-b xl:hidden'>{t('offer')}</h1>
-            <div className='flex flex-col xl:flex-row items-center xl:gap-20'>
-                <img src="/web/services/service.png" alt="Our Services" className='w-full xl:pb-0 xl:mb-0 xl:border-none sm:pb-8 sm:mb-8 sm:w-5/6 md:w-2/3 xl:w-7/12 pb-5 mb-5 border-b' />
-                <div className='flex gap-8 flex-col items-center justify-center xl:items-start w-full'>
-                    <h1 className='text-gray-700 text-3xl md:text-4xl lg:text-5xl font-light lg:font-extralight pb-8 mb-8 border-b'>{t('offer')}</h1>
-                    <div className='text-gray-700 text-center xl:text-left xl:items-start flex flex-col w-5/6 md:w-2/3 xl:w-full 2xl:w-5/6 items-center gap-3'>
-                        <img src="/web/services/service1.jpg" alt="Service 1" className='h-20 w-20' />
-                        <h1 className='font-medium text-xl text-black lg:text-2xl mt-auto'>{t('1.h1')}</h1>
-                        <p className='text-sm md:text-base'>{t('1.p')}</p>
+            <h1 className='text-gray-700 text-3xl md:text-4xl lg:text-5xl font-light lg:font-extralight pb-8 mb-8 border-b 2xl:hidden'>{t('offer')}</h1>
+            <div className='flex flex-col 2xl:flex-row 2xl:justify-around items-center xl:gap-20 w-full'>
+                <Image src={'/web/services/service.png'} alt='Our Services' width={730} height={730} />
+                {/* <img src="/web/services/service.png" alt="Our Services" className='w-full xl:pb-0 xl:mb-0 xl:border-none sm:pb-8 sm:mb-8 sm:w-5/6 md:w-2/3 xl:w-7/12 pb-5 mb-5 border-b' /> */}
+                <div className='flex gap-10 flex-col items-center justify-center 2xl:items-start 2xl:w-2/5'>
+                    <h1 className='text-gray-700 text-3xl md:text-4xl lg:text-5xl hidden 2xl:block font-light lg:font-extralight pb-8 border-b'>{t('offer')}</h1>
+                   
+                    <div className='flex items-center flex-col sm:flex-row lg:w-5/6 2xl:w-full gap-4 p-4 border'>
+                        <Image src={'/web/services/service1.jpg'} alt='Service 1' width={144} height={144} />
+                        <div className='text-gray-700 sm:text-left text-center 2xl:items-start flex gap-3 flex-col w-5/6 md:w-2/3 2xl:w-5/6'>
+                            <h1 className='font-medium text-xl text-black lg:text-2xl mt-auto'>{t('1.h1')}</h1>
+                            <p className='text-sm md:text-base'>{t('1.p')}</p>
+                        </div>
                     </div>
 
-                    <div className='text-gray-700 text-center xl:text-left xl:items-start flex flex-col w-5/6 md:w-2/3 xl:w-full 2xl:w-5/6 items-center gap-3'>
-                        <img src="/web/services/service2.jpg" alt="Service 2" className='h-20 w-20' />
-                        <h1 className='font-medium text-xl text-black lg:text-2xl mt-auto'>{t('2.h1')}</h1>
-                        <p className='text-sm md:text-base'>{t('2.p')}</p>
+                    <div className='flex items-center flex-col sm:flex-row lg:w-5/6 2xl:w-full gap-4 p-4 border'>
+                        <Image src={'/web/services/service2.jpg'} alt='Service 2' width={144} height={144} />
+                        <div className='text-gray-700 sm:text-left text-center 2xl:items-start flex gap-3 flex-col w-5/6 md:w-2/3 2xl:w-5/6'>
+                            <h1 className='font-medium text-xl text-black lg:text-2xl mt-auto'>{t('2.h1')}</h1>
+                            <p className='text-sm md:text-base'>{t('2.p')}</p>
+                        </div>
                     </div>
 
-                    <div className='text-gray-700 text-center xl:text-left xl:items-start flex flex-col w-5/6 md:w-2/3 xl:w-full 2xl:w-5/6 items-center gap-3'>
-                        <img src="/web/services/service3.jpg" alt="Service 3" className='h-20 w-20' />
-                        <h1 className='font-medium text-xl text-black lg:text-2xl mt-auto'>{t('3.h1')}</h1>
-                        <p className='text-sm md:text-base'>{t('3.p')}</p>
+                    <div className='flex items-center flex-col sm:flex-row lg:w-5/6 2xl:w-full gap-4 p-4 border'>
+                        <Image src={'/web/services/service3.jpg'} alt='Service 3' width={144} height={144} />
+                        <div className='text-gray-700 sm:text-left text-center 2xl:items-start flex gap-3 flex-col w-5/6 md:w-2/3 2xl:w-5/6'>
+                            <h1 className='font-medium text-xl text-black lg:text-2xl mt-auto'>{t('3.h1')}</h1>
+                            <p className='text-sm md:text-base'>{t('3.p')}</p>
+                        </div>
                     </div>
 
                 </div>

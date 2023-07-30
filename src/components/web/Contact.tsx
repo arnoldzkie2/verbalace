a7ec44c3379181e058febf94beeab01be2ac9c5e
arnoldzkie2 <arnoldzkie22@gmail.com>
2023-07-30 16:42:23 +0800

    added spamm protection

--- src/components/web/Contact.tsx
+++ src/components/web/Contact.tsx
@@ -1,17 +1,49 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 'use client'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faLocationDot, faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
-import { useTranslations } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface ContactProps {
 }
 
 const Contact: React.FC<ContactProps> = ({ }) => {
+
+    const [isSpamming, setIsSpamming] = useState(false)
+
+    const SPAM_TIMEOUT = 5 * 60 * 1000; // 5 minutes in milliseconds
+
+    const locale = useLocale()
+
+    useEffect(() => {
+
+        const contactTimeout = localStorage.getItem('contactTimeout');
+
+        if (contactTimeout) {
+
+            const elapsedTime = Date.now() - parseInt(contactTimeout);
+
+            if (elapsedTime < SPAM_TIMEOUT) {
+
+                setIsSpamming(true);
+
+                // Start a timer to reset isSpamming after 10 minutes
+                const timer = setTimeout(() => {
+                    setIsSpamming(false);
+                }, SPAM_TIMEOUT - elapsedTime);
+
+                return () => {
+                    clearTimeout(timer);
+                };
+            }
+        }
+
+    }, []);
 
     const [formData, setFormData] = useState({
         name: '',
@@ -49,13 +81,36 @@ const Contact: React.FC<ContactProps> = ({ }) => {
             })
 
             if (data.status) {
-                alert('Thank you for leaving a message')
+                localStorage.setItem('contactTimeout', Date.now().toString());
+
+                if (locale === 'en') {
+                    alert(`Thank you ${name} for leaving a message.`);
+                } else if (locale === 'ja') {
+                    alert(`${name}さん、メッセージを残していただきありがとうございます。`);
+                } else if (locale === 'kr') {
+                    alert(`${name}님, 메시지를 남겨주셔서 감사합니다.`);
+                } else if (locale === 'vi') {
+                    alert(`Cảm ơn ${name} đã để lại lời nhắn.`);
+                } else if (locale === 'zh') {
+                    alert(`谢谢您留言，${name}。`);
+                }
+
                 setFormData({
                     name: '',
                     phone: '',
                     email: '',
                     message: ''
                 })
+                setIsSpamming(true)
+
+                const timer = setTimeout(() => {
+                    setIsSpamming(false);
+                }, SPAM_TIMEOUT);
+
+                return () => {
+                    clearTimeout(timer);
+                };
+
             }
         } catch (error) {
 
@@ -63,6 +118,56 @@ const Contact: React.FC<ContactProps> = ({ }) => {
 
         }
     }
+
+    const formatTime = (time: number, locale: string) => {
+        const minutes = Math.floor(time / 60000);
+        const seconds = ((time % 60000) / 1000).toFixed(0);
+
+        if (locale === 'en') {
+            return `${minutes} minutes and ${seconds} seconds`;
+        } else if (locale === 'ja') {
+            return `${minutes}分${seconds}秒`;
+        } else if (locale === 'kr') {
+            return `${minutes}분 ${seconds}초`;
+        } else if (locale === 'vi') {
+            return `${minutes} phút và ${seconds} giây`;
+        } else if (locale === 'zh') {
+            return `${minutes} 分钟 ${seconds} 秒`;
+        }
+
+        return `${minutes} minutes and ${seconds} seconds`;
+    };
+
+
+    const spamm = async (e: any) => {
+        e.preventDefault();
+      
+        const contactTimeout = localStorage.getItem('contactTimeout');
+      
+        if (contactTimeout) {
+          const elapsedTime = Date.now() - parseInt(contactTimeout);
+          const remainingTime = SPAM_TIMEOUT - elapsedTime;
+      
+          if (remainingTime > 0) {
+            const formattedTime = formatTime(remainingTime, locale);
+            
+            if (locale === 'en') {
+              alert(`You already submitted a message. Please try again in ${formattedTime}`);
+            } else if (locale === 'ja') {
+              alert(`すでにメッセージを送信しています。${formattedTime}後にもう一度お試しください。`);
+            } else if (locale === 'kr') {
+              alert(`이미 메시지를 제출하셨습니다. ${formattedTime} 후에 다시 시도해주세요.`);
+            } else if (locale === 'vi') {
+              alert(`Bạn đã gửi một tin nhắn rồi. Vui lòng thử lại sau ${formattedTime}.`);
+            } else if (locale === 'zh') {
+              alert(`您已经提交了一条消息。请在${formattedTime}后再试。`);
+            } else {
+              alert(`You already submitted a message. Please try again in ${formattedTime}`);
+            }
+          }
+        }
+      };
+      
 
     return (
         <section id='contact' className='bg-[url(/web/contact/contact.svg)] bg-center bg-no-repeat bg-cover w-screen flex items-center justify-center py-10 lg:py-20 flex-col px-5 sm:px-10 md:px-16 lg:px-24 xl:px-36 2xl:px-44 gap-10 md:gap-16 lg:gap-24'>
@@ -95,7 +200,7 @@ const Contact: React.FC<ContactProps> = ({ }) => {
                     </div>
                 </aside>
 
-                <form onSubmit={sendMessage} className='flex flex-col p-7 border bg-white shadow-2xl rounded-3xl gap-3 w-full sm:w-96'>
+                <form onSubmit={isSpamming ? spamm : sendMessage} className='flex flex-col p-7 border bg-white shadow-2xl rounded-3xl gap-3 w-full sm:w-96'>
                     <input type="text" className='px-3 py-2 border-b border-gray-300 text-gray-600 outline-none' name='name' onChange={handleForm} value={formData.name} placeholder={t('form.name')} />
                     <input type="text" className='px-3 py-2 border-b border-gray-300 text-gray-600 outline-none' name='phone' onChange={handleForm} value={formData.phone} placeholder={t('form.phone')} />
                     <input type="text" className='px-3 py-2 border-b border-gray-300 text-gray-600 outline-none' name='email' onChange={handleForm} value={formData.email} placeholder={t('form.email')} />
